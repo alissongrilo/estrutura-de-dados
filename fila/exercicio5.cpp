e149b2adafb24ba9d6e072300b77cf33f9405f17
alissongrilo <alissonmiguel197@gmail.com>
2022-11-23 20:41:58 -0300

    exercicio 5

---
+++ fila/exercicio5.cpp
@@ -0,0 +1,107 @@
+typedef int Dado;
+#include <iostream>
+#include <string>
+using namespace std;
+
+class filaCircular {
+    private:
+        int mInicio;
+        int mFim;
+        Dado *mDados;
+        unsigned mCapacidade;
+        unsigned mTamanho;
+    public:
+        filaCircular(int capacidade = 10);
+        ~filaCircular();
+        unsigned tamanho();
+        void enfileira(Dado valor);
+        Dado desenfileira();
+        bool vazia();
+        Dado pegaInicio();
+};
+
+filaCircular::filaCircular(int capacidade) {
+    mInicio = -1;
+    mFim = -1;
+    mCapacidade = capacidade;
+    mDados = new Dado[mCapacidade];
+    mTamanho = 0;
+}
+
+filaCircular::~filaCircular() {
+    delete [] mDados;
+}
+
+unsigned filaCircular::tamanho() {
+    return mTamanho;
+}
+
+bool filaCircular::vazia() {
+    return (mTamanho == 0);
+}
+
+void filaCircular::enfileira(Dado valor) {
+    if (mTamanho <= mCapacidade) {
+        mFim++;
+        mFim = mFim % mCapacidade;
+        mDados[mFim] = valor;
+        
+        mTamanho == 0 ? mInicio++ : mInicio = mInicio;
+        mTamanho++;
+    } else {
+        cout<<"Fila cheia"<<endl;
+    }
+}
+
+Dado filaCircular::desenfileira() {
+    if (mTamanho > 0) {
+        Dado valor = mDados[mInicio];
+        mTamanho--;
+        if (mTamanho > 0) {
+            mInicio++;
+            mInicio = mInicio % mCapacidade;
+        } else {
+            mInicio = -1;
+            mFim = -1;
+        }
+        return valor;
+    } else {
+        cout<<"Fila vazia"<<endl;
+        return -1;
+    }
+}
+
+Dado filaCircular::pegaInicio() {
+    return mDados[mInicio];
+}
+
+int main() {
+    int qtdNumerosBinarios;
+    Dado elemento;
+    string auxiliar;
+
+    cin>>qtdNumerosBinarios;
+
+    filaCircular f1(qtdNumerosBinarios + 2);
+    
+    f1.enfileira(1);
+    
+    for (unsigned i = 0; i < qtdNumerosBinarios; i++) {
+        elemento = f1.pegaInicio();
+        auxiliar = std::to_string(elemento);
+        auxiliar = auxiliar + "0";
+        elemento = std::stoi(auxiliar);
+        f1.enfileira(elemento);
+
+        elemento = f1.pegaInicio();
+        auxiliar = std::to_string(elemento);
+        auxiliar = auxiliar + "1";
+        elemento = std::stoi(auxiliar);
+        f1.enfileira(elemento);
+
+        cout<<f1.pegaInicio()<<" ";
+        f1.desenfileira();
+    }
+
+    f1.~filaCircular();
+}
